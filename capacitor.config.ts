4d8011eec03e2aeacfeadba0e509854c6069f93a
jsalazar1401 <utm19010146@utma.edu.mx>
2023-02-22 17:52:21 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'minicop-Hackaton',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
