e137f9e54b7b7152ac554d538dc2c2b4d9ea4a83
jsalazar1401 <utm19010146@utma.edu.mx>
2023-02-23 19:13:46 -0600

    Todas las paginas termindas

--- src/app/contactos/contactos.page.ts
+++ src/app/contactos/contactos.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-contactos',
@@ -7,7 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactosPage implements OnInit {
 
-  constructor() { }
+  handlerMessage = '';
+  roleMessage = '';
+
+  constructor(private alertController: AlertController) { }
+  
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            this.handlerMessage = 'Alert canceled';
+          },
+        },
+        {
+          text: 'OK',
+          role: 'confirm',
+          handler: () => {
+            this.handlerMessage = 'Alert confirmed';
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    this.roleMessage = `Dismissed with role: ${role}`;
+  }
 
   ngOnInit() {
   }
--- src/app/mensajes/mensajes.page.ts
+++ src/app/mensajes/mensajes.page.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-mensajes',
   templateUrl: './mensajes.page.html',
   styleUrls: ['./mensajes.page.scss'],
 })
-export class MensajesPage implements OnInit {
+export class MensajesPage{
+  mensaje1 = '';
+  mensaje2 = '';
+  mensaje3 = '';
 
-  constructor() { }
+  constructor(private toastController: ToastController) {
+   
+  }
+  
+  registrarMensajes(forma: NgForm) {
+    console.log(forma.value)
+    this.mensaje1 = forma.value.mensaje1;
+    this.mensaje2 = forma.value.mensaje2;
+    this.mensaje3 = forma.value.mensaje3;
+   
+  }
 
-  ngOnInit() {
+  async presentToast(position: 'bottom') {
+    const toast = await this.toastController.create({
+      message: 'Mensajes guardados',
+      duration: 2000,
+      position: position,
+      
+    });
+
+    await toast.present();
   }
 
 }
